d81933fef8213f4e5acff6b4f6f011ecac1de135
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-12 16:00:31 -0300

    documento

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cpf_cnpj import CpfCnpj
 
 exemplo_cnpj = "35379838000112"
-documento = CpfCnpj(exemplo_cnpj, 'cnpj')
+exemplo_cpf = "49589179851"
+documento = CpfCnpj(exemplo_cpf, 'cpf')
+#documento = CpfCnpj(exemplo_cpf, 'cnpj')
 print(documento)
